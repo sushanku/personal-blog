0f75739131625992a29209e05daf5a137148648a
sushanku <sushankunwar01@gmail.com>
2024-07-24 17:34:54 +0545

    Starting my personal blog

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/about', title: 'About' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/projects', title: 'Projects' },
+  
+]
+
+export default headerNavLinks
